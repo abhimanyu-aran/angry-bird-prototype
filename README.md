4dd5cd2aecf0a23fc5d9436d1ec177dd44988c7c
abhimanyu-aran <72918626+abhimanyu-aran@users.noreply.github.com>
2020-11-15 11:45:28 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# angry-bird-prototype
+# TopplingBoxes
+Toppling Boxes
---
+++ sketch.js
@@ -0,0 +1,38 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(200,300,50,50);
+    box2 = new Box(240,100,50,100);
+    ground = new Ground(600,height,1200,20)
+    pig1 = new Pig(570,204)
+    log1 = new Log(522,276,100,PI)
+    log2 = new Log(619,276,100,PI)
+    log3 = new Log(570,240,130,PI/2)
+    bird1 = new Bird(700,200)
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    text(mouseX +":" + mouseY,1000,100)
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+    log2.display();
+    log3.display();
+    bird1.display();
+}
